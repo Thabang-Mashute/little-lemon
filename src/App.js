3a9cfc686e372a6feebcb8cd510d5454b21a2c1c
TMASHUT <thabang.mashute@mercedes-benz.com>
2025-07-10 16:06:40 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import React from "react";
+import Header from "./Header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div>
-      <b>Homepage</b>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© {new Date().getFullYear()} Little Lemon. All rights reserved.</p>
+      <p>123 Lemon Street, Flavor Town</p>
+    </footer>
+  );
+}
+
+export default Footer;
